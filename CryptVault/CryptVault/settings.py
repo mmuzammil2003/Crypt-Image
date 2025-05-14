aa19f1d1e299e54ff25fc596a4e3fef076dc1fc3
mmuzammil2003 <muzammilamir03@gmail.com>
2025-05-15 00:10:52 +0530

    io

--- CryptVault/CryptVault/settings.py
+++ CryptVault/CryptVault/settings.py
@@ -149,8 +149,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'studypurpose8758@gmail.com'
-EMAIL_HOST_PASSWORD = 'tvqc pafo ivdp vjiu'  # Use App Password, not your actual Gmail password
+EMAIL_HOST_USER = #######
+EMAIL_HOST_PASSWORD =  # Use App Password, not your actual Gmail password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
